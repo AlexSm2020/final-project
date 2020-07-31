eaf924f612d85364997af8def705e2f658e76a16
Sedat Atakan <satakan35@gmail.com>
2020-07-30 21:04:27 -0400

    commit

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -6,7 +6,12 @@ class About extends Component {
   render() {
     return (
       <div>
-        <h1>About Us Page</h1>
+        <h1>About Us</h1>
+        <p>Andrew Murphy<a href="https://www.linkedin.com/in/b-andrew-murphy/">here is you can contact to me</a></p>
+        <p>Aref Smiley<a href="https://www.linkedin.com/in/aref-smiley-61941757/">here is you can contact to me</a></p>
+        <p>John Rademaker<a href="https://www.linkedin.com/in/rademakerjohn1/">here is you can contact to me</a></p>
+        <p>Sedat Atakan<a href="https://www.linkedin.com/in/sedat-atakan/">here is you can contact to me</a></p>
+        
       </div>
     )
   }
