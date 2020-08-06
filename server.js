f43bf970c3de4b3c6602d634825c99a1c3cb04a1
Andrew Murphy <amurphy37@gmail.com>
2020-08-06 17:22:59 -0400

    updated server.js to test deploy

--- server.js
+++ server.js
@@ -45,10 +45,10 @@ app.use(passport.session());
 // Step 3
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname + "client/public/index.html"))
     });
 }
 
