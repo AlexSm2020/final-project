d0916a8e4b73a206d9c1e7f4c514981add1e6246
Andrew Murphy <amurphy37@gmail.com>
2020-08-06 16:17:47 -0400

    updated server.js for heroku route issue

--- server.js
+++ server.js
@@ -46,6 +46,10 @@ app.use(passport.session());
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+    });
 }
 
 // HTTP request logger
