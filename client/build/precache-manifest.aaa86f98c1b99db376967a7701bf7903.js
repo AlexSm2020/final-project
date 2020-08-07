716cb43a0b19966bb526d57b48a2a45d5abc999b
Andrew Murphy <amurphy37@gmail.com>
2020-08-07 14:42:13 -0400

    ran npm build and updated server.js

---
+++ client/build/precache-manifest.aaa86f98c1b99db376967a7701bf7903.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2078e5302fcd49a37376340ca7bb02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05395870ac859c01e28b",
+    "url": "/static/css/2.3bf28fc6.chunk.css"
+  },
+  {
+    "revision": "c87dbb357fe428ed74a4",
+    "url": "/static/css/main.7bb6ec3a.chunk.css"
+  },
+  {
+    "revision": "05395870ac859c01e28b",
+    "url": "/static/js/2.dde05538.chunk.js"
+  },
+  {
+    "revision": "d39714e1b941a480462aecfa296c1c20",
+    "url": "/static/js/2.dde05538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c87dbb357fe428ed74a4",
+    "url": "/static/js/main.8e78abbe.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
