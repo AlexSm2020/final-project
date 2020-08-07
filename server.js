0ed10af789833e39bdaf14d023f24058db036bc1
Andrew Murphy <amurphy37@gmail.com>
2020-08-07 14:51:43 -0400

    edited path for serving

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const PORT = process.env.PORT || 8080; // Step 1
 
 // Production Mode
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, ".", "client", 'build')));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, '.', 'client', 'build', 'index.html'))
     });
 }
 
