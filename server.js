cab7080a62db0959a59de7183f6bf1e17ef0080d
Andrew Murphy <amurphy37@gmail.com>
2020-08-09 13:02:52 -0400

    updated server.js

--- server.js
+++ server.js
@@ -54,10 +54,6 @@ const inProduction = process.env.NODE_ENV === "production"
 // Production Mode
 if (inProduction) {
     app.use(express.static('client/build'))
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    });
 }
 
 // const { createProxyMiddleware } = require('http-proxy-middleware')
